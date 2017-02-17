777bd3480d7ada426556b493ca7a49727d130447
chetan <bairagisirfire@gmail.com>
2017-02-17 21:15:00 +0530

    added explanation

--- src/main/java/com/programmingfree/springservice/Application.java
+++ src/main/java/com/programmingfree/springservice/Application.java
@@ -10,9 +10,19 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
 
+/*
+The @EnableJpaRepositories annotation activates Spring Data JPA.
+ Spring Data JPA will create a concrete implementation of the TaskRepository
+ and configure it to talk to a back end in-memory database using JPA.
+ */
 @Configuration
 @ComponentScan
 @EnableJpaRepositories
+/*
+@Import(RepositoryRestMvcConfiguration.class) annotation imports a collection of Spring MVC controllers,
+JSON converters, and other beans needed to provide a RESTful front end.
+These components link up to the Spring Data JPA backend.
+*/
 @Import(RepositoryRestMvcConfiguration.class)
 @EnableAutoConfiguration
 @PropertySource("application.properties")
--- src/main/java/com/programmingfree/springservice/repository/TaskRepository.java
+++ src/main/java/com/programmingfree/springservice/repository/TaskRepository.java
@@ -7,9 +7,24 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+/*
+TaskRepository extends CrudRepository. The type of entity and ID it works with,
+Task and Long are specified as generic parameters to CrudRepository.
+ By extending CrudRepository, TaskRepository inherits several methods
+ for working with Task persistence, including methods for saving, deleting, and finding Task entities.
+ */
 @RepositoryRestResource
-public interface TaskRepository extends CrudRepository<Task, Integer> {
+/*
+ou can see @RepositoryRestResource annotation being used.
+This annotation is responsible for exposing this repository interface as a RESTFul resource.
+ This is pretty much similar to @RestController which we used in plain Spring MVC REST to expose a
+ controller as RESTFul resource.
 
+ */
+public interface TaskRepository extends CrudRepository<Task, Integer> {
+/*
+Spring Data JPA also allows you to define other query methods by simply declaring their method signature.
+ */
 	List<Task> findByTaskArchived(@Param("archivedfalse") int taskArchivedFalse);
 	List<Task> findByTaskStatus(@Param("status") String taskStatus);
 
